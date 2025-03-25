c087636c998358c73b0090791c02e4962f7bdbb5
Witold Paprotny <witold.paprotny@vattenfall.com>
2025-03-25 09:44:35 +0100

    add header comment to schemas and format schemas output in schemas.js

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ import fs from 'fs'
 import path from 'path'
 import { callChatGPT } from './gptHelpers.js'
 
+const schemasHeader = `// Schemas from Swagger (api-docs/components/schemas) goes in here.
+// Do not modify schemas in here. Do that in another files.
+// Overwrite the schemas with _.merge if you need to modify them.
+// Example src/components/Users/Form/schema.js
+
+export const schemas = {`
+
 // Function to call the first API and then the second API
 async function callApisAndSaveResponse() {
   try {
@@ -12,11 +19,15 @@ async function callApisAndSaveResponse() {
     const response1 = await fetch(swaggerUrl)
     const swagger = await response1.json()
     const { schemas } = swagger.components
-
     console.log('- I have Schemas!')
     // Save the response from the first API as a file in the home directory
     const fileSchemasPath = path.join(process.env.HOME, 'schemas.js')
-    fs.writeFileSync(fileSchemasPath, JSON.stringify(schemas, null, 2))
+    const schemasWithHeader = JSON.stringify(schemas, null, 2).replace(
+      '{',
+      schemasHeader
+    )
+
+    fs.writeFileSync(fileSchemasPath, schemasWithHeader)
     console.log('- schemas saved to', fileSchemasPath)
 
     console.log('2. Calling GPT --------------------------------')
