11c262f6bc574a9797fbb78d77d51e8c52bc76e2
Witold Paprotny <witold.paprotny@vattenfall.com>
2025-04-02 11:11:19 +0200

    add dotenv support and update file paths for schema and types output; Now it is possible to run the script with npx

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+#!/usr/bin/env node
+import dotenv from 'dotenv'
 import fetch from 'node-fetch'
 import fs from 'fs'
 import path from 'path'
+import { fileURLToPath } from 'url'
 import { callChatGPT } from './gptHelpers.js'
 
 const schemasHeader = `// Schemas from Swagger (api-docs/components/schemas) goes in here.
@@ -13,15 +16,16 @@ export const schemas = {`
 // Function to call the first API and then the second API
 async function callApisAndSaveResponse() {
   try {
+    const rootFolder = path.join(process.cwd(), process.env.ROOT_FILES_PATH)
     console.log('1. Calling Swagger --------------------------------')
     const swaggerUrl = process.env.SWAGGER_URL
-
+    console.log('- Swagger url', swaggerUrl)
     const response1 = await fetch(swaggerUrl)
     const swagger = await response1.json()
     const { schemas } = swagger.components
     console.log('- I have Schemas!')
-    // Save the response from the first API as a file in the home directory
-    const fileSchemasPath = path.join(process.env.HOME, 'schemas.js')
+    // Save the response from the first API as a file
+    const fileSchemasPath = path.join(rootFolder, 'schemas.js')
     const schemasWithHeader = JSON.stringify(schemas, null, 2).replace(
       '{',
       schemasHeader
@@ -40,8 +44,8 @@ async function callApisAndSaveResponse() {
       .replaceAll('```', '')
       .trim()
 
-    // Save the response from the second API as a file in the home directory
-    const filePath = path.join(process.env.HOME, 'types.d.ts')
+    // Save the response from the second API as a file.
+    const filePath = path.join(rootFolder, 'types.d.ts')
     fs.writeFileSync(filePath, types)
 
     console.log('Response saved to', filePath)
@@ -49,6 +53,14 @@ async function callApisAndSaveResponse() {
     console.error('Error:', error)
   }
 }
+// const pathLocal = path.resolve(process.cwd(), '.env')
+
+// Emulate the __dirname and __filename variables in ES modules
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const pathLocal = path.resolve(__dirname, '.env')
+// Load environment variables from the .env file
+dotenv.config({ path: pathLocal })
 
 // Call the function
 callApisAndSaveResponse()
